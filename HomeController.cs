98b22ace1bfa0870dcfe3d637fd62220ed1a4292
glorialwang <111816444+glorialwang@users.noreply.github.com>
2023-01-02 00:10:00 -0600

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wang_Gloria_HW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //TODO: Create a new action method for the new page
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            string path = _environment.WebRootPath + "/files/GloriaWang_Resume.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "GloriaWang_Resume.pdf");
+        }
+    }
+}
+
